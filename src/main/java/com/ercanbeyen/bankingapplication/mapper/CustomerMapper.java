b067f864895e783ad45e5865f4507585f389d0a8
Ercan Beyen <96631352+ercanbeyen@users.noreply.github.com>
2023-11-26 17:26:35 +0300

    Fix mapper issue

--- src/main/java/com/ercanbeyen/bankingapplication/mapper/CustomerMapper.java
+++ src/main/java/com/ercanbeyen/bankingapplication/mapper/CustomerMapper.java
@@ -3,9 +3,12 @@ package com.ercanbeyen.bankingapplication.mapper;
 import com.ercanbeyen.bankingapplication.dto.CustomerDto;
 import com.ercanbeyen.bankingapplication.entity.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
+    @Mapping(target = "addressDto", source = "address")
     CustomerDto customerToDto(Customer customer);
+    @Mapping(target = "address", source = "addressDto")
     Customer dtoToCustomer(CustomerDto customerDto);
 }
