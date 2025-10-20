f802da420fdc1109f29d62c0b31083f1678812b0
Ercan Beyen <96631352+ercanbeyen@users.noreply.github.com>
2025-10-20 16:07:19 +0300

    Add getAgreements endpoint

--- src/main/java/com/ercanbeyen/bankingapplication/service/AgreementService.java
+++ src/main/java/com/ercanbeyen/bankingapplication/service/AgreementService.java
@@ -4,9 +4,12 @@ import com.ercanbeyen.bankingapplication.dto.AgreementDto;
 import com.ercanbeyen.bankingapplication.entity.Customer;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface AgreementService {
     AgreementDto createAgreement(String subject, MultipartFile request);
     AgreementDto updateAgreement(String id, String subject, MultipartFile request);
+    List<AgreementDto> getAgreements();
     AgreementDto getAgreement(String id);
     void addCustomerToAgreement(String subject, Customer customer);
     String deleteAgreement(String id);
