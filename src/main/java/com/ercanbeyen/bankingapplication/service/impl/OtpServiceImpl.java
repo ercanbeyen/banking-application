c2913c483d632d5f30ed33857f49669f38319402
Ercan Beyen <96631352+ercanbeyen@users.noreply.github.com>
2026-06-30 16:06:13 +0300

    Add brute-force attack protection to OTP service

--- src/main/java/com/ercanbeyen/bankingapplication/service/impl/OtpServiceImpl.java
+++ src/main/java/com/ercanbeyen/bankingapplication/service/impl/OtpServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ercanbeyen.bankingapplication.service.impl;
 
+import com.ercanbeyen.bankingapplication.constant.message.LogMessage;
 import com.ercanbeyen.bankingapplication.service.OtpService;
 import com.ercanbeyen.bankingapplication.util.AuthUtil;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,8 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class OtpServiceImpl implements OtpService {
     private static final String OTP_KEY_PREFIX = "otp:";
+    private static final String ATTEMPTS_KEY_PREFIX = "otp_attempts:";
+    private static final int MAX_OTP_ATTEMPTS = 3;
 
     private final StringRedisTemplate redisTemplate;
     private final SecureRandom secureRandom = new SecureRandom();
@@ -23,6 +26,7 @@ public class OtpServiceImpl implements OtpService {
     public String generateOtp(String email) {
         String otp = String.format("%06d", secureRandom.nextInt(999999));
         String otpKey = OTP_KEY_PREFIX + email;
+        String attemptsKey = ATTEMPTS_KEY_PREFIX + email;
 
         redisTemplate.opsForValue().set(
                 otpKey,
@@ -31,21 +35,43 @@ public class OtpServiceImpl implements OtpService {
                 TimeUnit.MINUTES
         );
 
+        redisTemplate.delete(attemptsKey);
+
         return otp;
     }
 
     @Override
     public boolean validateOtp(String email, String otp) {
         String otpKey = OTP_KEY_PREFIX + email;
+        String attemptsKey = ATTEMPTS_KEY_PREFIX + email;
         String cachedOtp = redisTemplate.opsForValue().get(otpKey);
 
         if (cachedOtp == null) { // No OTP or expired
+            redisTemplate.delete(attemptsKey);
             return false;
         }
 
         if (cachedOtp.equals(otp)) { // match --> validation success
             redisTemplate.delete(otpKey);
+            redisTemplate.delete(attemptsKey);
             return true;
+        }
+
+        log.info("Before:: Redis has key: {}", redisTemplate.hasKey(attemptsKey));
+
+        /* Wrong OTP */
+        Long attempts = redisTemplate.opsForValue().increment(attemptsKey);
+        log.info("Attempts: {}", attempts);
+
+        log.info("After:: Redis has key: {}", redisTemplate.hasKey(attemptsKey));
+
+        if (attempts == null) { // Unknown OTP
+            log.warn(LogMessage.RESOURCE_NOT_FOUND, "OTP");
+        } else if (attempts == 1) { // set expiry time of otp attempts counter
+            redisTemplate.expire(attemptsKey, AuthUtil.getOtpValidMinutes(), TimeUnit.MINUTES);
+        } else if (attempts >= MAX_OTP_ATTEMPTS) { // invalidate the OTP
+            redisTemplate.delete(otpKey);
+            redisTemplate.delete(attemptsKey);
         }
 
         return false;
